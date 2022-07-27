5addce6d7b18818bf98a71e3f36771e3794b1ad8
elievelton <elievelton@gmail.com>
2022-07-27 17:29:44 -0300

    Conexão com firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import firebase from './firebaseConnection';
+import React, { useState } from 'react';
+import './style.css';
+
 
 function App() {
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  async function handleAdd(){
+    await firebase.firestore().collection('usuario').doc('122212').set({
+      nome: nome,
+      email: email,
+      senha: senha
+    }).then(()=>{
+      console.log('Dados cadastrados com sucesso!')
+    }).catch((error)=>{
+      console.log("Gerou Erro" + error)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testando banco de dados Firebase</h1>
+      <label> 
+          Nome:
+      </label>
+      
+      <input type="text" value={nome} onChange={(e)=>setNome(e.target.value)}/>
+      <label> 
+          E-mail:
+      </label>
+      <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <label> 
+          Senha:
+      </label>
+      <input type="text" value={senha} onChange={(e)=>setSenha(e.target.value)}/>
+      <button onClick={handleAdd}>Cadastrar</button>
     </div>
   );
 }
