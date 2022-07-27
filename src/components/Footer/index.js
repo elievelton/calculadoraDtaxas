67729b2ccbf5662231716cbc0b59a9db702a2afc
elievelton <elievelton@gmail.com>
2022-07-27 19:46:29 -0300

    Algumas correções das rotas

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <div>
+            <h1>Footer da Página</h1>
+        </div>
+
+    );
+}
+export default Footer;
--- src/routes.js
+++ src/routes.js
@@ -7,17 +7,25 @@ import Contato from './pages/Contato'
 import Ofertas from './pages/Ofertas'
 import Login from './pages/Login'
 
+//importando so componentes da página
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 // Fazendo as configurações das rotas do site
 
 function RoutesApp(){
     return (
         <BrowserRouter>
+        <Header/>
+        <Navbar/>
             <Routes>
                 <Route path='/' element = {<Home/>}/>
                 <Route path='/contato' element = {<Contato/>}/>
                 <Route path='/ofertas' element = {<Ofertas/>}/>
                 <Route path='/login' element = {<Login/>}/>
             </Routes>
+        <Footer/>
         </BrowserRouter>
     )
 }
