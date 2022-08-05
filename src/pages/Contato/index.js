7aa526b8574ab931f6085f5d16db75f4929f62dd
elievelton <elievelton@gmail.com>
2022-08-04 22:13:07 -0300

    formulario

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,10 +1,71 @@
+import styles from "../Contato/Contato.module.css";
+import React, { useState, useRef } from "react";
+import Alert from "@mui/material/Alert";
+import emailjs from "@emailjs/browser";
 
 function Contato() {
-    return(
-        <div>
-            <h1>Página de Contato</h1>
-        </div>
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensagem, setMensagem] = useState("");
+  const [sub, setSub] = useState(false);
 
-    );
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+
+    emailjs
+      .sendForm(
+        "calculadoradeTxas",
+        "template_3lrqp3l",
+        form.current,
+        "1wslfI6JpR1pcnXHc"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+      
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>Entre em Contato</h2>
+      <div className={styles.formu}>
+        <form ref={form} className="form-control" onSubmit={sendEmail} >
+          <input
+            onChange={(e) => setNome(e.target.value)}
+            type="text"
+            value={nome}
+            require
+            placeholder="Seu Nome"
+            name="nome"></input>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            value={email}
+            require
+            placeholder="Digite seu E-mail"
+            name="email"></input>
+          <textarea
+            onChange={(e) => setMensagem(e.target.value)}
+            value={mensagem}
+            name="mensagem"
+            require
+            placeholder="Digite sua mensagem"
+          />
+          <button className="btn" type="submit" name="submit">
+            Enviar mensagem
+          </button>
+          <Alert severity="success">This is a success alert — check it out!</Alert>
+        </form>
+      </div>
+    </div>
+  );
 }
 export default Contato;
