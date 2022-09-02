4dd797a5194f013f47d51e13012bdd3590a9e4e9
elievelton <elievelton@gmail.com>
2022-09-02 17:32:10 -0300

    Bd_funcionando

--- servidor/index.js
+++ servidor/index.js
@@ -5,8 +5,8 @@ const { EmailJSResponseStatus } = require('@emailjs/browser');
     const Usuario = require('./models/Usuario');
     const Plano = require('./models/Plano');
     const Empresa = require('./models/Empresa');
-    const Taxas = require('./models/Taxas');
-    const Recebimento = require('./models/Recebimento');
+    //const Taxas = require('./models/Taxas');
+    //const Recebimento = require('./models/Recebimento');
     
     await db.sequelize.sync();
 
@@ -15,5 +15,53 @@ const { EmailJSResponseStatus } = require('@emailjs/browser');
         email:"carlosvicto@gmail.com",
         senha:"123456",
     })
+    const novaEmpresa = await Empresa.create({
+        nome:"Ton",
+        notaReclameAqui: 8.6,
+        melhoremque:"Melhor no Débito"
+
+    })
+
+    const novoPlano = await Plano.create({
+        nome:"Plano 1",
+        nome_qtd_dias:"14",
+        pessoa:"Fisica",
+        fatordetaxa:1.5,
+        debito:1.1,
+        Credito:4.6,
+        Credito2x:6.0,
+        Credito3x:7.0,
+        Credito4x:8.0,
+        Credito5x:9.0,
+        Credito6x:10.0,
+        Credito7x:11.0,
+        Credito8x:12.0,
+        Credito9x:13.0,
+        Credito10x:14.0,
+        Credito11x:15.0,
+        Credito12x:16.0,
+        EmpresaId:novaEmpresa.id,
+    }) 
+    
+    const novoPlano2 = await Plano.create({
+        nome:"Plano 2",
+        nome_qtd_dias:"14",
+        pessoa:"Fisica",
+        fatordetaxa:1.5,
+        debito:1.1,
+        Credito:4.6,
+        Credito2x:6.0,
+        Credito3x:7.0,
+        Credito4x:8.0,
+        Credito5x:9.0,
+        Credito6x:10.0,
+        Credito7x:11.0,
+        Credito8x:12.0,
+        Credito9x:13.0,
+        Credito10x:14.0,
+        Credito11x:15.0,
+        Credito12x:16.0,
+        EmpresaId:novaEmpresa.id,
+    })  
 
 })();
--- servidor/models/Empresa.js
+++ servidor/models/Empresa.js
@@ -1,36 +1,24 @@
-const db = require('../db');
-const Plano = require('./Plano');
-const Empresa = db.sequelize.define('empresa',{
-    id:{
-        type: db.Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-
-    },
-    nome:{
-        type: db.Sequelize.STRING,
-        allowNull: false,
-        
-
-    },
-    notaReclameAqui:{
-        type: db.Sequelize.DECIMAL,
-        allowNull: false,
-        
-
-    },
-    melhoremque:{
-        type: db.Sequelize.STRING,
-        allowNull: false,
-        
-
-    }
+const db = require("../db");
+const Plano = require("./Plano");
+const Empresa = db.sequelize.define("empresa", {
+  id: {
+    type: db.Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  nome: {
+    type: db.Sequelize.STRING,
+    allowNull: false,
+  },
+  notaReclameAqui: {
+    type: db.Sequelize.DECIMAL,
+    allowNull: false,
+  },
+  melhoremque: {
+    type: db.Sequelize.STRING,
+    allowNull: false,
+  },
 });
 
-Empresa.sync();
-Empresa.belongsTo(Plano,{
-    constraint : true,
-    foreiKey: 'idPlano',
-})
 module.exports = Empresa;
