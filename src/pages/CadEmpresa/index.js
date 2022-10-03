dacabe4b92712cb0ae56ea0efb0b695c22a018a8
elievelton <elievelton@gmail.com>
2022-10-03 17:09:54 -0300

    Atualizações cadastro e listar

--- src/pages/CadEmpresa/index.js
+++ src/pages/CadEmpresa/index.js
@@ -2,32 +2,51 @@ import React from "react";
 import style from "./CadEmpresa.module.css";
 import { useInsertDocument } from "../../hooks/insertEmpresa";
 import { useAuthValue } from "../../context/AuthContext";
+import { ToastContainer, toast } from "react-toastify";
 
 const CadEmpresa = ({ className }) => {
   const [nome, setNome] = React.useState("");
   const [notaReclameAqui, setNotaReclameAqui] = React.useState("");
-  const [melhoremque, setmelhoremque] = React.useState();
+  const [melhoremque, setmelhoremque] = React.useState("");
+  const [bannerUrl, setBannerUrl] = React.useState("");
+  const [link, setLink] = React.useState("");
   const [errorForm, setErrorForm] = React.useState("");
   const { user } = useAuthValue();
 
   const { insertDocument, response } = useInsertDocument("empresas");
 
+  //notificação de cadastro de empresa
+  const notify = () =>
+    toast.success("Cadastro feito com sucesso!", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
   //Função de enviar o formulario
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrorForm("");
-    let ids = "";
+
     insertDocument({
       nome,
       notaReclameAqui,
       melhoremque,
+      bannerUrl,
+      link,
     });
+    notify();
   };
 
   return (
+    <>
     <div className={`${style.cadEmp} ${className}`}>
       <h2>Cadastrar empresa</h2>
-      <form >
+      <form>
         <label htmlFor="nome">
           Nome da empresa:
           <input
@@ -38,10 +57,32 @@ const CadEmpresa = ({ className }) => {
             onChange={(e) => setNome(e.target.value)}
           />
         </label>
+        <label htmlFor="url">
+          Url da imagem:
+          <input
+            type="text"
+            name="url"
+            placeholder="(Link da imagem da máquina de cartão)"
+            id=""
+            value={bannerUrl}
+            onChange={(e) => setBannerUrl(e.target.value)}
+          />
+        </label>
+        <label htmlFor="link">
+          Link do site:
+          <input
+            type="text"
+            name="link"
+            placeholder="(Link do site)"
+            id=""
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
+        </label>
         <label htmlFor="destak">
           Destaque em que?
           <input
-            type="number"
+            type="text"
             name="destak"
             id=""
             placeholder="(deixar em branco se não houver)"
@@ -51,33 +92,49 @@ const CadEmpresa = ({ className }) => {
         </label>
         <label htmlFor="nota">
           Nota do reclame aqui:
-          <select
-            name="nota"
-            id="nota"
-            value={notaReclameAqui}
-            onChange={(e) => setNotaReclameAqui(e.target.value)}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
+          <input
+                  
+                  type="number"
+                  name="nota"
+                  id="nota"
+                  placeholder="ex: 9.4"
+                  
+                  value = {notaReclameAqui}
+                  onChange={(e) => {
+                    setNotaReclameAqui(e.target.value);
+                  }}
+                />
         </label>
+        
+        
       </form>
-      {!response.loading && <button onClick={handleSubmit} className="btn">Cadastrar</button>}
+      
+      {!response.loading && (
+        <button onClick={handleSubmit} className="btn">
+          Cadastrar
+        </button>
+      )}
       {response.loading && (
-        <button  className="btn" disabled>
+        <button className="btn" disabled>
           aguarde...
         </button>
       )}
 
       {response.error && <p className="error">{response.error}</p>}
-    </div>
+      </div>
+      <ToastContainer
+            theme="colored"
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+      </>
   );
 };
 
--- src/pages/ListarPlanos/index.js
+++ src/pages/ListarPlanos/index.js
@@ -5,26 +5,29 @@ import Relatorio from "../Relatorio";
 import style from "./Listar.module.css"
 import { db } from "../../firebase/config";
 import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import {FaTrash} from "react-icons/fa";
+
+
 
 const ListarPlanos = ({className}) => {
   const empresaRef = localStorage.getItem('empresa')
   const [dataPlanos, setDataPlanos] = useState([]);
-  const [empresas, setEmpresas] = useState([]);
 
+  const [buscaPlanos, setBuscaPlanos] = useState([]);
+
+
+  
    // busca no banco de dados do firebase
-   const empresaCollectionRef = collection(db, "empresas");
+  
    const planoCollectionRef = collection(db, "planos");
  
    useEffect(() => {
      const getEmpresas = async () => {
-       const lista_empresas = await getDocs(empresaCollectionRef);
+       
        const lista_planos = await getDocs(planoCollectionRef);
  
-       setEmpresas(
-         lista_empresas.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-       );
+       
        setDataPlanos(
          lista_planos.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
        );
@@ -32,22 +35,20 @@ const ListarPlanos = ({className}) => {
      getEmpresas();
    }, []);
  
-   //Filtragem dos dados planos
-   const nomeDoPlanos = dataPlanos.map((plano) => plano.nome);
-   const reference = dataPlanos.map((plano) => plano.reference);
-   const tipo = dataPlanos.map((plano) => plano.tipo);
-   const taxas1 = dataPlanos.map((plano) => plano.recebimento1);
-   const taxas2 = dataPlanos.map((plano) => plano.recebimento15);
-   const taxas3 = dataPlanos.map((plano) => plano.recebimento30);
- 
-   //filtragem dos dados Empresa
- 
-   const nomeDaEmpresa = empresas.map((empre) => empresas.nome);
-   const notaReclame = empresas.map((empre) => empresas.notaReclameAqui);
-   const melhorEm = empresas.map((empre) => empresas.melhorEmQue);
+  
+
+   const BuscaPlanoFiltro = () => {
+   const data = dataPlanos.filter((plano) =>
+      plano.reference.startsWith(empresaRef.toLowerCase())
+    );
+    setBuscaPlanos(data);
+  };
+  
+
  
    //Funções para deletar empresa e plano precisa apenas do id
   async function deletePlano(id) {
+
     const userDoc = doc(db, "planos", id);
     await deleteDoc(userDoc);
   }
@@ -56,22 +57,31 @@ const ListarPlanos = ({className}) => {
     await deleteDoc(userDoc);
   }
   //deleteEmpresa('ywAoTNmx6EDNC4MWRMew')
+  useEffect(() => {
+    BuscaPlanoFiltro();
+    
+  }, [dataPlanos]);
+
+
 
   return (
     <div className={`${style.lista} ${className}`}>
       <h2>Planos Cadastradas da <span>{empresaRef}</span></h2>
       <ul>
-        {empresas.map(empresa => {
-            return <li key={empresa.id} className={style.item}>
-                <p>{empresa.nome}</p> 
+        {buscaPlanos.map(plan => {
+            return <li key={plan.id} className={style.item}>
+                <p>{plan.nome}</p> 
                 <div className={style.buttons}>
-                    <Link to={'/paineldecontrole/listar/planos/edit'} onClick={(target)=>{localStorage.setItem('plano',empresa.nome)}}>Editar Plano</Link>
-                    <button style={{backgroundColor: '#ff8d8d'}}><FaTrash/></button>
+                    <Link to={'/paineldecontrole/listar/planos/edit'} onClick={({target})=>{localStorage.setItem('plano',plan.nome)}}>Editar Plano</Link>
+                    <button onClick={()=>{
+                      deletePlano(plan.id)
+                    }} style={{backgroundColor: '#ff8d8d'}}><FaTrash/></button>
                 </div>
             </li>
         })}
       </ul>
     </div>
+    
   );
 };
 
